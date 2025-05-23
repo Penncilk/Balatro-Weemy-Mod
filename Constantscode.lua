735e6ff8b112f7fbf41cd7e9dbb6db1ef9181262
Sine System <judeuyeno@gmail.com>
2025-05-23 15:59:54 -0600

    I'm never learning my god damn lesson aren't I

--- Constantscode.lua
+++ Constantscode.lua
@@ -135,7 +135,7 @@ SMODS.Consumable {
     end,
 
     can_use = function(self, card)
-        if #G.hand.highlighted <= card.ability.select then
+        if (#G.hand.highlighted <= card.ability.select) and (#G.hand.highlighted >= 0) then
             return true
         end
     end,
